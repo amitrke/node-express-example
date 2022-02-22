1478805bdd05a25b865e79f641bbbf97c017a95a
amitrke <amitrke@gmail.com>
2022-02-22 05:29:18 +0000

    Users page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var john = {
+  firstName: "John",
+  lastName: "King",
+  email: "john.king@yahoo.com"
+};
+
+var mike = {
+  firstName: "Mike",
+  lastName: "Reid",
+  email: "mike.reid@msn.com"
+}
+
+var users = [john, mike];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource, this is for the users page.');
+  res.render('userlist', { title: 'User List', users });
 });
 
 module.exports = router;
